1e3b2a307cf83accd829ca8605b6aa2fb362f47c
Clement Parisot <clement.parisot@inria.fr>
2016-01-20 11:20:48 +0100

    [nancy] graoully - Add pdu sensors

--- generators/input/sites/nancy/clusters/graoully/graoully.rb
+++ generators/input/sites/nancy/clusters/graoully/graoully.rb
@@ -224,6 +224,19 @@ site :nancy do |site_uid|
           :gpu  => false
         })
 
+        sensors({
+          :power => {
+            :available => true,
+            :via => { 
+              :api => { :metric => 'power' },
+              :pdu => [ {
+                :uid => lookup('graoully_manual', node_uid, 'pdu', 'pdu_name'),
+                :port => lookup('graoully_manual', node_uid, 'pdu', 'pdu_position'),
+              } ]
+            }
+          }
+        })
+
       end
     end
   end # cluster graoully
