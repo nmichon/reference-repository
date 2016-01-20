d253b0aba7503ecb9671464b03addbbe51a9a3b1
Clement Parisot <clement.parisot@inria.fr>
2016-01-20 11:03:54 +0100

    [nancy] pdus - Update nancy pdus

--- generators/input/sites/nancy/pdus.rb
+++ generators/input/sites/nancy/pdus.rb
@@ -55,7 +55,7 @@ site :nancy do |site_uid|
     end
   end
   2.times do |i|
-    pdu "mbi-pdu#{i+1}" do |pdu_uid|
+    pdu "griffon-pdu#{i+1}" do |pdu_uid|
       vendor "APC"
       model "AP7953"
       sensors [
@@ -66,6 +66,63 @@ site :nancy do |site_uid|
               :available => true,
               :total_oids => ["iso.3.6.1.4.1.318.1.1.12.1.16.0"],
               :unit => "W"
+            }
+          }
+        }
+      ]
+    end
+  end
+  2.times do |i|
+    pdu "graoully-pdu#{i+1}" do |pdu_uid|
+      vendor "APC"
+      model "AP8653"
+      sensors [
+        {
+          :power => {
+            :per_outlets => true,
+            :snmp => {
+              :available => true,
+              :total_oids => ["iso.3.6.1.4.1.318.1.1.12.1.16.0"],
+              :unit => "W",
+              :outlet_prefix_oid => "iso.3.6.1.4.1.318.1.1.26.9.4.3.1.7"
+            }
+          }
+        }
+      ]
+    end
+  end
+  2.times do |i|
+    pdu "grimoire-pdu#{i+1}" do |pdu_uid|
+      vendor "APC"
+      model "AP8653"
+      sensors [
+        {
+          :power => {
+            :per_outlets => true,
+            :snmp => {
+              :available => true,
+              :total_oids => ["iso.3.6.1.4.1.318.1.1.12.1.16.0"],
+              :unit => "W",
+              :outlet_prefix_oid => "iso.3.6.1.4.1.318.1.1.26.9.4.3.1.7"
+            }
+          }
+        }
+      ]
+    end
+  end
+  2.times do |i|
+    pdu "grisou-pdu#{i+1}" do |pdu_uid|
+      vendor "APC"
+      model "AP8653"
+      sensors [
+        {
+          :power => {
+            :per_outlets => true,
+            :snmp => {
+              :available => true,
+              :total_oids => ["iso.3.6.1.4.1.318.1.1.12.1.16.0"],
+              :unit => "W",
+              :outlet_prefix_oid => "iso.3.6.1.4.1.318.1.1.26.9.4.3.1.7"
             }
           }
         }
